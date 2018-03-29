8d48a3dd9166586d84553bb611cfd10933430ee1
Carlos Vaz <carjorvaz@gmail.com>
2018-03-29 11:06:24 +0100

    Qual. 2015-2017 - 17A - 100 pts.

---
+++ ONI2018/mooshakTreino/qual2015-2017/17A/17Asol4.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+  int n, b, i, encontrados = 0;
+  cin >> n >> b >> i;
+  b -= 1;
+
+  string arcas;
+  cin >> arcas;
+
+  int maxE = b, maxD = b;
+
+  for (int x = 0; x < i; x++) {
+    char d;
+    cin >> d;
+
+    int p;
+    cin >> p;
+
+    int sentido = (d == 'E' ? -1 : 1);
+
+    p *= sentido;
+
+    b += p;
+
+    if (sentido == -1 && b < maxE) {
+      maxE = b;
+    }
+
+    if (sentido == 1 && b > maxD) {
+      maxD = b;
+    }
+  }
+
+  for (int x = maxE; x <= maxD; x++) {
+    if (arcas[x] == 'T') {
+      encontrados++;
+    }
+  }
+
+  cout << encontrados << endl;
+
+
+  return 0;
+}
